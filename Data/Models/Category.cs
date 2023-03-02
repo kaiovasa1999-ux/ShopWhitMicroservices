6a17b341ffdb515644ce1b6856ff84def19f2a30
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2023-03-02 13:29:58 +0200

    data models and project initial create

---
+++ Data/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace Shop.Data.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Descirption { get; set; }
+
+        public IEnumerable<ItemAd> Items { get; set; } = new List<ItemAd>();
+
+    }
+}
---
+++ Data/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Shop.Data.Models
+{
+    public class User : IdentityUser
+    {
+        public Dealer Dealer{ get; set; }
+    }
+}
